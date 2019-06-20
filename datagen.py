a538f5a5ac6d1aa059a23e0aabf8e82c62f18415
americast <sayan.sinha@iitkgp.ac.in>
2019-06-20 10:52:02 -0400

    Segregate

---
+++ datagen.py
@@ -0,0 +1,67 @@
+from skimage.io import imread
+from skimage.transform import resize
+import numpy as np
+import random
+
+def get_data(image_filenames, type_dict, files, input_shape, batch_size, tv = 't'):
+    batch_x = image_filenames
+    j = 0
+    pos = True
+    X = []
+    Y = []
+    count = 0
+    while j < len(batch_x):
+        # print str(j)+"/"+str(len(batch_x))
+        label = 1
+        if (count == 0 and pos):
+            X = []
+            Y = []
+        each = batch_x[j]
+        if tv == 't':
+            img = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each), (input_shape[0], input_shape[1]))
+        else:
+            img = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each), (input_shape[0], input_shape[1]))
+        # pu.db
+
+        range_here = type_dict[each.split("_")[0]]
+        if pos:
+            while True:
+                ind = random.randrange(*range_here)
+                each_2 = files[ind]
+                if (each_2 != each): break
+            
+            if tv == 't':
+                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each_2), (input_shape[0], input_shape[1]))
+            else:
+                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each_2), (input_shape[0], input_shape[1]))
+            pos = False
+        else:
+            count +=1
+            
+            while True:
+                ind = random.randrange(0,len(files))
+                if ind not in range(*range_here): break
+
+            each_2 = files[ind]
+            if tv == 't':
+                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each_2), (input_shape[0], input_shape[1]))
+            else:
+                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each_2), (input_shape[0], input_shape[1]))
+            
+            label = 0
+            pos = True
+            j+=1
+
+        # pu.db
+
+        X.append(np.concatenate((img.reshape(1, input_shape[0], input_shape[1], 3), img_2.reshape(1, input_shape[0], input_shape[1], 3)), axis = 0))
+        Y.append(label)
+
+
+
+        # batch_y = labels[idx * batch_size:(idx + 1) * batch_size]
+
+        if (count == batch_size):
+            X_arr = np.array(X)
+            count = 0
+            yield ([X_arr[:,0], X_arr[:,1]], np.array(Y))
--- train.py
+++ train.py
@@ -23,6 +23,8 @@ import os
 import random
 from copy import copy
 import pudb
+from datagen import *
+from utils import *
 
 EPOCHS = 10000
 batch_size = 56
@@ -30,91 +32,6 @@ batch_size = 56
 #os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 input_shape = (224, 224, 3)
-
-def get_data(image_filenames, type_dict, files, tv = 't'):
-    batch_x = image_filenames
-    j = 0
-    pos = True
-    X = []
-    Y = []
-    count = 0
-    while j < len(batch_x):
-        # print str(j)+"/"+str(len(batch_x))
-        label = 1
-        if (count == 0 and pos):
-            X = []
-            Y = []
-        each = batch_x[j]
-        if tv == 't':
-            img = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each), (input_shape[0], input_shape[1]))
-        else:
-            img = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each), (input_shape[0], input_shape[1]))
-        # pu.db
-
-        range_here = type_dict[each.split("_")[0]]
-        if pos:
-            while True:
-                ind = random.randrange(*range_here)
-                each_2 = files[ind]
-                if (each_2 != each): break
-            
-            if tv == 't':
-                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each_2), (input_shape[0], input_shape[1]))
-            else:
-                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each_2), (input_shape[0], input_shape[1]))
-            pos = False
-        else:
-            count +=1
-            
-            while True:
-                ind = random.randrange(0,len(files))
-                if ind not in range(*range_here): break
-
-            each_2 = files[ind]
-            if tv == 't':
-                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each_2), (input_shape[0], input_shape[1]))
-            else:
-                img_2 = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each_2), (input_shape[0], input_shape[1]))
-            
-            label = 0
-            pos = True
-            j+=1
-
-        # pu.db
-
-        X.append(np.concatenate((img.reshape(1, input_shape[0], input_shape[1], 3), img_2.reshape(1, input_shape[0], input_shape[1], 3)), axis = 0))
-        Y.append(label)
-
-
-
-        # batch_y = labels[idx * batch_size:(idx + 1) * batch_size]
-
-        if (count == batch_size):
-            X_arr = np.array(X)
-            count = 0
-            yield ([X_arr[:,0], X_arr[:,1]], np.array(Y))
-
-            
-
-def euclidean_distance(vects):
-    x, y = vects
-    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
-    return K.sqrt(K.maximum(sum_square, K.epsilon()))
-
-
-def eucl_dist_output_shape(shapes):
-    shape1, shape2 = shapes
-    return (shape1[0], 1)
-
-
-def contrastive_loss(y_true, y_pred):
-    '''Contrastive loss from Hadsell-et-al.'06
-    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-    '''
-    margin = 1
-    square_pred = K.square(y_pred)
-    margin_square = K.square(K.maximum(margin - y_pred, 0))
-    return K.mean(y_true * square_pred + (1 - y_true) * margin_square)
 
 def create_base_network(in_dim):
     """ Base network to be shared (eq. to feature extraction).
@@ -136,17 +53,6 @@ def create_base_network(in_dim):
     return Model(inputs=model.input, outputs=model.get_layer('fc1000').output)
 
 
-def compute_accuracy(y_true, y_pred):
-    '''Compute classification accuracy with a fixed threshold on distances.
-    '''
-    pred = y_pred.ravel() < 0.5
-    return np.mean(pred == y_true)
-
-
-def accuracy(y_true, y_pred):
-    '''Compute classification accuracy with a fixed threshold on distances.
-    '''
-    return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
 #### Training images
 files = os.listdir("../VeRi/VeRi_with_plate/image_train")
@@ -237,7 +143,7 @@ checkpointer = ModelCheckpoint(monitor='acc', filepath="check.h5", verbose=True,
 print("Here")
 # pu.db
 if tv == 'v' or tv == 'V':
-    print(model_gpu.evaluate_generator(get_data(files_perm_val, type_dict_val, files_val, 'v'), steps=(num_validation_samples * 2 // (batch_size * 4)), use_multiprocessing=True, workers=16, max_queue_size=32))
+    print(model_gpu.evaluate_generator(get_data(files_perm_val, type_dict_val, files_val, input_shape, batch_size, 'v'), steps=(num_validation_samples * 2 // (batch_size * 4)), use_multiprocessing=True, workers=16, max_queue_size=32))
 else:
     for _ in xrange(EPOCHS):
         print _
@@ -249,11 +155,11 @@ else:
         #       print("orig: "+str(x[1]))
 
             
-        model_gpu.fit_generator(generator=get_data(files_perm, type_dict, files),
+        model_gpu.fit_generator(generator=get_data(files_perm, type_dict, files, input_shape, batch_size),
                                             steps_per_epoch=(num_training_samples * 2 // (batch_size * 4)),
                                             epochs=1,
                                             verbose=1,
-                                            validation_data=get_data(files_perm_val, type_dict_val, files_val, 'v'),
+                                            validation_data=get_data(files_perm_val, type_dict_val, files_val, input_shape, batch_size,'v'),
                                             validation_steps=(num_validation_samples // batch_size * 4),
                                             use_multiprocessing=True,
                                             workers=16,
@@ -261,6 +167,3 @@ else:
 
         model.save_weights("check_weights.h5")
         #model_gpu.save("check.h5")
-
-
-
