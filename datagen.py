88af7e5470829ac97a37f98508be85b94c7f6f52
americast <sayan.sinha@iitkgp.ac.in>
2019-06-27 17:43:42 -0400

    WIP train class...looks good :stuckout_tongue:

--- datagen.py
+++ datagen.py
@@ -129,6 +129,54 @@ def get_data_hot(image_filenames, type_dict, files, input_shape, batch_size, tv 
             count = 0
             yield ([X_arr[:,0], X_arr[:,1]], np.array(Y))
 
+def get_data_hot_unit(image_filenames, type_dict, files, input_shape, batch_size, tv = 't'):
+    batch_x = image_filenames
+    j = 0
+    X = []
+    Y = []
+    count = 0
+    while True:
+        j = 0
+        while j < len(batch_x):
+            # print str(j)+"/"+str(len(batch_x))
+            label = [1.0]
+            # print("In datagen")
+            if (count == 0):
+                X = []
+                Y = []
+            each = batch_x[j]
+            if tv == 't':
+                img = resize(imread("../VeRi/VeRi_with_plate/unit/"+each), (input_shape[0], input_shape[1]))
+            else:
+                img = resize(imread("../VeRi/VeRi_with_plate/unit/"+each), (input_shape[0], input_shape[1]))
+            # pu.db
+            
+            # pu.db
+            img = img.reshape(img.shape[0], img.shape[1], 1)
+            img = np.concatenate((img, img, img), axis = -1)
+
+            X.append(img.reshape(input_shape[0], input_shape[1], 3))
+            num = each.split("_")[0]
+            if num == "0":
+                label = [1, 0, 0]
+            elif num == "1":     
+                label = [0, 1, 0]
+            else:
+                label = [0, 0, 1]
+            Y.append(label)
+
+            count+=1
+            j+=1
+
+            # batch_y = labels[idx * batch_size:(idx + 1) * batch_size]
+
+            if (count == batch_size):
+                X_arr = np.array(X)
+                count = 0
+                yield X_arr.astype("float32"), np.array(Y).astype("float32")
+
+
+
 def get_data_ratio(image_filenames, type_dict, files, input_shape, batch_size, ratio = 0.1, tv = 't'):
     batch_x = image_filenames
     j = 0
@@ -172,6 +220,67 @@ def get_data_ratio(image_filenames, type_dict, files, input_shape, batch_size, r
                 img_2 = resize(imread("../VeRi/VeRi_with_plate/image_train/"+each_2), (input_shape[0], input_shape[1]))
             else:
                 img_2 = resize(imread("../VeRi/VeRi_with_plate/image_test/"+each_2), (input_shape[0], input_shape[1]))
+            
+            label = 1
+        j+=1
+
+        # pu.db
+
+        X.append(np.concatenate((img.reshape(1, input_shape[0], input_shape[1], 3), img_2.reshape(1, input_shape[0], input_shape[1], 3)), axis = 0))
+        Y.append(label)
+
+
+
+        # batch_y = labels[idx * batch_size:(idx + 1) * batch_size]
+
+        if (count == batch_size):
+            X_arr = np.array(X)
+            count = 0
+            yield ([X_arr[:,0], X_arr[:,1]], np.array(Y))
+
+def get_data_ratio_wild(image_filenames, type_dict, files, input_shape, batch_size, ratio = 0.1, tv = 't'):
+    batch_x = image_filenames
+    j = 0
+    X = []
+    Y = []
+    count = 0
+    while j < len(batch_x):
+        print (str(j)+"/"+str(len(batch_x)))
+        label = 0
+        if (count == 0):
+            X = []
+            Y = []
+        each = batch_x[j]
+        if tv == 't':
+            img = resize(imread("../VeRI_Wild/images/images/"+each+".jpg"), (input_shape[0], input_shape[1]))
+        else:
+            img = resize(imread("../VeRI_Wild/images/images/"+each+".jpg"), (input_shape[0], input_shape[1]))
+        # pu.db
+
+        range_here = type_dict[each.split("/")[0]]
+        count +=1
+        if random.random() <= ratio:
+            while True:
+                ind = random.randrange(*range_here)
+                each_2 = files[ind]
+                if (each_2 != each): break
+            
+            if tv == 't':
+                img_2 = resize(imread("../VeRI_Wild/images/images/"+each_2+".jpg"), (input_shape[0], input_shape[1]))
+            else:
+                img_2 = resize(imread("../VeRI_Wild/images/images/"+each_2+".jpg"), (input_shape[0], input_shape[1]))
+
+        else:
+            
+            while True:
+                ind = random.randrange(0,len(files))
+                if ind not in range(*range_here): break
+
+            each_2 = files[ind]
+            if tv == 't':
+                img_2 = resize(imread("../VeRI_Wild/images/images/"+each_2+".jpg"), (input_shape[0], input_shape[1]))
+            else:
+                img_2 = resize(imread("../VeRI_Wild/images/images/"+each_2+".jpg"), (input_shape[0], input_shape[1]))
             
             label = 1
         j+=1
